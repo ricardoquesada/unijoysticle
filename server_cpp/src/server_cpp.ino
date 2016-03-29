50d025052427e65ded053ae371c2d0d249bab91f
Ricardo Quesada <ricardoquesada@gmail.com>
2016-03-28 23:05:29 -0700

    feat: add udp client. server works

--- server_cpp/src/server_cpp.ino
+++ server_cpp/src/server_cpp.ino
@@ -1,76 +1,163 @@
+/****************************************************************************
+Copyright 2016 Ricardo Quesada
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+****************************************************************************/
+
+// based on http://www.esp8266.com/viewtopic.php?f=29&t=2222
+
 #include <Arduino.h>
-
-
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+#include <WiFiUDP.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "queque2";
-const char* password = "locopajaro";
-MDNSResponder mdns;
-
-ESP8266WebServer server(80);
-
-const int led = 13;
-
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+extern "C" {  //required for read Vdd Voltage
+#include "user_interface.h"
+    // uint16 readvdd33(void);
 }
 
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+int status = WL_IDLE_STATUS;
+const char* ssid = "queque2";           //  your network SSID (name)
+const char* pass = "locopajaro";        // your network password
+const unsigned int localPort = 6464;    // local port to listen for UDP packets
+
+MDNSResponder mdns;                     // announce Joystick service
+
+// believe or not, they are not ordered
+static const int pinsPort0[] = {D1, D2, D3, D4, D5};
+static const int pinsPort1[] = {D6, D7, D8, D9, D10};
+static const int TOTAL_PINS = sizeof(pinsPort0) / sizeof(pinsPort0[0]);
+
+byte packetBuffer[512];             //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+
+void setup()
+{
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+
+    // setting up Station AP
+    WiFi.begin(ssid, pass);
+
+    // Wait for connect to AP
+    Serial.print("[Connecting]");
+    Serial.print(ssid);
+    int tries=0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        tries++;
+        if (tries > 30) {
+            Serial.println("Could not connect. Check SSID and password");
+            break;
+        }
+    }
+    Serial.println();
+
+    printWifiStatus();
+
+    Serial.println("Connected to wifi");
+    Serial.print("Udp server started at port ");
+    Serial.println(localPort);
+    Udp.begin(localPort);
+
+    if (mdns.begin("c64joy", WiFi.localIP()))
+        Serial.println("MDNS responder started");
+
+    for (int i=0; i<TOTAL_PINS; i++)
+    {
+        pinMode(pinsPort0[i], OUTPUT);
+        digitalWrite(pinsPort0[i], LOW);
+        pinMode(pinsPort1[i], OUTPUT);
+        digitalWrite(pinsPort1[i], LOW);
+    }
+
+    // Internal LED in LoLin
+    pinMode(D0, OUTPUT);
+    digitalWrite(D0, HIGH);  // turn it off
 }
 
-void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+void loop()
+{
+    int noBytes = Udp.parsePacket();
+    String received_command = "";
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("hola! Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    // byte 0:
+    // LSB Nibble: Control Port: 0 or 1
+    // MSB Nibble: 0 Joystick value
+    //             1 Mouse (Not supported yet)
+    //             2 Paddle (Not supported yet)
+    //
+    // byte 1:
+    // Joystick  bit 0: up
+    //           bit 1: down
+    //           bit 2: left
+    //           bit 3: right
+    //           bit 4: fire
+    if (noBytes == 2)
+    {
+        // DEBUG: turn on internal LED
+        digitalWrite(D0, LOW);
 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-  }
+        // Serial.print(millis() / 1000);
+        // Serial.print(":Packet of ");
+        // Serial.print(noBytes);
+        // Serial.print(" received from ");
+        // Serial.print(Udp.remoteIP());
+        // Serial.print(":");
+        // Serial.println(Udp.remotePort());
+        // We've received a packet, read the data from it
+        Udp.read(packetBuffer,noBytes); // read the packet into the buffer
 
-  server.on("/", handleRoot);
+        const int* pins;
+        if (packetBuffer[0] == 0)
+            pins = pinsPort0;
+        else
+            pins = pinsPort1;
 
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
+        for (int i=0; i<TOTAL_PINS; i++)
+        {
+            if (packetBuffer[1] & (1<<i))
+                digitalWrite(pins[i], HIGH);
+            else
+                digitalWrite(pins[i], LOW);
+        }
 
-  server.onNotFound(handleNotFound);
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.write("Answer from ESP8266 ChipID#");
+        Udp.print(system_get_chip_id());
+        Udp.write("#IP of ESP8266#");
+        Udp.println(WiFi.localIP());
+        Udp.endPacket();
 
-  server.begin();
-  Serial.println("HTTP server started");
+        // Serial.println(received_command);
+        // Serial.println();
+
+        // DEBUG: turn off internal LED
+        digitalWrite(D0, HIGH);
+    }
 }
 
-void loop(void){
-  server.handleClient();
+void printWifiStatus()
+{
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 }
