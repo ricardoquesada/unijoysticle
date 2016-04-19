ed3177bd62cfbe22063b49903fff915dcdc5ecdc
Ricardo Quesada <ricardoquesada@gmail.com>
2016-04-19 11:14:33 -0700

    feat: add up/down support as well
    uses `userRotation` instead of the accelerometer. data is more reliable.

--- client_ios/src/UniGamesScene.swift
+++ client_ios/src/UniGamesScene.swift
@@ -9,13 +9,12 @@
 import SpriteKit
 import CoreMotion
 
-
 class UniGamesScene: SKScene {
 
     // assign nodes to buttons
     enum JoyBits: UInt8 {
-        case Top    = 0b00000001
-        case Bottom = 0b00000010
+        case Up     = 0b00000001
+        case Down   = 0b00000010
         case Left   = 0b00000100
         case Right  = 0b00001000
         case Fire   = 0b00010000
@@ -28,6 +27,7 @@ class UniGamesScene: SKScene {
     let net = NetworkConnection()
 
     var motionManager = CMMotionManager()
+    var rotationRate = CMRotationRate();
 
     // accel tmp
     var labelX:SKLabelNode? = nil
@@ -37,7 +37,7 @@ class UniGamesScene: SKScene {
     var labelBack:SKLabelNode? = nil
     var zMax:Double = -100
     var zMin:Double = 100
-    let accelFilter = HighpassFilter(sampleRate: 60, cutoffFrequency: 5.0)
+//    let accelFilter = HighpassFilter(sampleRate: 60, cutoffFrequency: 5.0)
 
     let operationQueue = NSOperationQueue()
 
@@ -46,23 +46,27 @@ class UniGamesScene: SKScene {
         // accelerometer stuff
         if motionManager.accelerometerAvailable == true {
 
-            accelFilter.adaptive = false
+//            accelFilter.adaptive = false
             labelX = childNodeWithName("SKLabelNode_x") as! SKLabelNode?
             labelY = childNodeWithName("SKLabelNode_y") as! SKLabelNode?
             labelZ = childNodeWithName("SKLabelNode_z") as! SKLabelNode?
             labelState = childNodeWithName("SKLabelNode_state") as! SKLabelNode?
             labelBack = childNodeWithName("SKLabelNode_back") as! SKLabelNode?
 
-            motionManager.accelerometerUpdateInterval = 1/60
-            motionManager.startAccelerometerUpdatesToQueue(operationQueue, withHandler:{
+//            motionManager.accelerometerUpdateInterval = 1/60
+//            motionManager.startAccelerometerUpdatesToQueue(operationQueue, withHandler:{
+//                data, error in
+//
+//                self.accelFilter.addAcceleration(data!.acceleration)
+//
+//                self.zMin = min(data!.acceleration.z, self.zMin)
+//                self.zMax = max(data!.acceleration.z, self.zMax)
+//            })
+
+            motionManager.startDeviceMotionUpdatesToQueue(operationQueue, withHandler: {
                 data, error in
-
-                self.accelFilter.addAcceleration(data!.acceleration)
-
-                self.zMin = min(data!.acceleration.z, self.zMin)
-                self.zMax = max(data!.acceleration.z, self.zMax)
+                self.rotationRate = data!.rotationRate
             })
-
         }
     }
 
@@ -77,27 +81,44 @@ class UniGamesScene: SKScene {
 
     override func update(currentTime: CFTimeInterval) {
 
-        self.labelX!.text = String(format:"x = %.2f", accelFilter.x)
-        self.labelY!.text = String(format:"y = %.2f", accelFilter.y)
-        self.labelZ!.text = String(format:"z = %.2f", accelFilter.z)
+        self.labelX!.text = String(format:"x = %.2f", rotationRate.x)
+        self.labelY!.text = String(format:"y = %.2f", rotationRate.y)
+        self.labelZ!.text = String(format:"z = %.2f", rotationRate.z)
         self.labelState!.text = pad(String(joyState, radix: 2), toSize: 8)
 
 //        print("min: \(zMin), max: \(zMax)")
         print(self.labelState!.text)
+//        print(self.rotationRate);
 
-        // accel Z (up and down) controls joy left & right for the unicycle game
-        // Accel.Z > 0 == Joy Left
-        // Accel.Z < 0 == Joy Right
+        // accel Z (up and down) controls joy Up & Down for the unicycle game
+        // Accel.Z > 0 == Joy Up
+        // Accel.Z < 0 == Joy Down
         let threshold = 0.5
-        if (accelFilter.z < -threshold) {
+        if (rotationRate.x < -threshold) {
+            joyState &= ~JoyBits.Down.rawValue
+            joyState |= JoyBits.Up.rawValue
+        }
+        else if (rotationRate.x > threshold) {
+            joyState &= ~JoyBits.Up.rawValue
+            joyState |= JoyBits.Down.rawValue
+        }
+        else if (rotationRate.x > -0.05 && rotationRate.x < 0.05) {
+            // not moving. turn off up and down
+            joyState &= ~(JoyBits.Up.rawValue | JoyBits.Down.rawValue)
+        }
+
+        // accel X (left and right) controls joy Left & Right for the unicycle game
+        // Accel.X > 0 == Joy Right
+        // Accel.X < 0 == Joy Left
+        if (rotationRate.z < -threshold) {
             joyState &= ~JoyBits.Left.rawValue
             joyState |= JoyBits.Right.rawValue
         }
-        else if (accelFilter.z > threshold) {
+        else if (rotationRate.z > threshold) {
             joyState &= ~JoyBits.Right.rawValue
             joyState |= JoyBits.Left.rawValue
         }
-        else if (accelFilter.z > -0.05 && accelFilter.z < 0.05) {
+        else if (rotationRate.z > -0.05 && rotationRate.z < 0.05) {
             // not moving. turn off left and right
             joyState &= ~(JoyBits.Right.rawValue | JoyBits.Left.rawValue)
         }
