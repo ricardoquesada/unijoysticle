95e691ecf93ca6bbba679c91a8b8ae0b48d9559f
Ricardo Quesada <ricardoquesada@gmail.com>
2016-09-17 06:56:21 -0700

    android client: analytics gets initialized

--- client_android/app/src/main/java/moe/retro/unijoysticle/MainActivity.java
+++ client_android/app/src/main/java/moe/retro/unijoysticle/MainActivity.java
@@ -39,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // init analytics
+        AnalyticsTrackers.initialize(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
